820f07c1f6806a7fff62732b64689ba17292357b
Toby Ya <35889113+fdsf53451001@users.noreply.github.com>
2022-01-26 13:54:56 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # -*- coding:utf-8 -*-
-
+from cv2 import add
 import smbus
-bus = smbus.SMBus(1)
+from mlx90614 import MLX90614
+
+# bus = smbus.SMBus(1)
 address = 0x5a
 
 
 if __name__ == '__main__':
-    for i in range(40):
-        try:
-            print(i,bus.read_byte_data(address,i))
-        except Exception:
-            print('Error happened!')
+    # for i in range(40):
+    #     try:
+    #         print(i,bus.read_byte_data(address,i))
+    #     except Exception:
+    #         print('Error happened!')
+    thermo = MLX90614(address)
+    print(thermo.get_obj_temp())
