135efeae9cc1a2cc9a632ac4568e456f22d78763
Toby Ya <35889113+fdsf53451001@users.noreply.github.com>
2022-02-09 12:03:49 +0800

    update

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 # -*- coding:utf-8 -*-
 import smbus
+import RPi.GPIO as GPIO
 from mlx90614 import MLX90614
 
-# bus = smbus.SMBus(1)
-address = 0x5a
+import time
+
+# setup thermo
+temp1_address = 0x5a
+thermo1 = MLX90614(temp1_address)
+
+# setup servo motor
+servo1_pin = 12
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo1_pin, GPIO.OUT)
+motor1 = GPIO.PWM(servo1_pin, 50) # GPIO 17 for PWM with 50Hz
+
+def get_temp(thermo,address):
+    try:
+        return thermo.get_obj_temp()
+    except IOError as e:
+        print('temp sensor error!')
+        return -1
+
+def move_motor(motor,angle):
+    motor.ChangeDutyCycle(5)
+    time.sleep(0.5)
 
 
 if __name__ == '__main__':
-    # for i in range(40):
-    #     try:
-    #         print(i,bus.read_byte_data(address,i))
-    #     except Exception:
-    #         print('Error happened!')
-    thermo = MLX90614(address)
-    print(thermo.get_obj_temp())
+
+    temp1 = get_temp(thermo1,temp1_address)
+    print(temp1)
+
+    move_motor(motor1,5)
+    move_motor(motor1,10)
+    move_motor(motor1,5)
+
+
+    
