f7995dc1351da3f0b3f13d155e814681c563c843
Toby Ya <35889113+fdsf53451001@users.noreply.github.com>
2022-02-16 14:36:20 +0800

    update

--- test_motor.py
+++ test_motor.py
@@ -17,10 +17,13 @@ wiringpi.pwmSetRange(2000)
 
 delay_period = 0.01
 
-while True:
-        for pulse in range(50, 250, 1):
-                wiringpi.pwmWrite(18, pulse)
-                time.sleep(delay_period)
-        for pulse in range(250, 50, -1):
-                wiringpi.pwmWrite(18, pulse)
-                time.sleep(delay_period)
+# while True:
+#         for pulse in range(50, 250, 1):
+#                 wiringpi.pwmWrite(18, pulse)
+#                 time.sleep(delay_period)
+#         for pulse in range(250, 50, -1):
+#                 wiringpi.pwmWrite(18, pulse)
+#                 time.sleep(delay_period)
+
+wiringpi.pwmWrite(18, 150)
+time.sleep(delay_period)
